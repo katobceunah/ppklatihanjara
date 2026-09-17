8f3cc43b414ff48f117c8352c55b6f752468c9ad
einarvern <adefritze.s1806@gmail.com>
2026-09-18 02:17:26 +0700

    feat(ui): integrasi tampilan frontend
    Membuat halaman login, dashboard, dan menyambungkan visualisasi progress bar

--- todoapp/server.js
+++ todoapp/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+app.use(express.static('public')); // Baris baru untuk menampilkan index.html
 
 // --- DATABASE DUMMY ---
 const users = [];
